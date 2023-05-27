83a3937403cbf8f129b6b740ced81708071cf2af
sina rastgouyan <sina.rastgouyan@gmail.com>
2023-05-27 19:39:21 +0430

    UserAdmin Class Has Been Customized By CustomUserAdmin Class Definition.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age', )}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age', )}),
+    )
+
+
