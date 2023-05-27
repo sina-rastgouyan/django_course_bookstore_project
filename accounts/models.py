8dca3c8da68b9688f3858fc2415b1a25b9f8f9c0
sina rastgouyan <sina.rastgouyan@gmail.com>
2023-05-27 20:19:31 +0430

    CustomUser age Attribute Edited and null, blank Set For It.

--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField()
-    
+    age = models.PositiveIntegerField(null=True, blank=True)
+
