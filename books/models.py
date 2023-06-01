4ead3d0a5ba46262cb173a9ba88f3513eb2026fc
sina rastgouyan <sina.rastgouyan@gmail.com>
2023-06-01 16:09:01 +0430

    Book Model Implemented.

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=350)
+    author = models.CharField(max_length=350)
+    description = models.TextField() 
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.title
