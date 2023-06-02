631a7b9b30b5ddcf63c9ff722bf7e37e2e470162
sina rastgouyan <sina.rastgouyan@gmail.com>
2023-06-03 02:02:38 +0430

    Basic Settings Configurations Done For Using Static Files. Static Files Put in Project But have not applied yet.

--- config/settings.py
+++ config/settings.py
@@ -121,11 +121,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -143,3 +138,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Config Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
