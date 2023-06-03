e664beeeca05a461077303b13c3ff2a9427847e9
sina rastgouyan <sina.rastgouyan@gmail.com>
2023-06-03 18:31:41 +0430

    settings & static imported in urls file and MEDIA_ROOT & MEDIA_URL added to urlpatterns.

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
@@ -24,4 +27,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('books/', include('books.urls'))
-]
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
