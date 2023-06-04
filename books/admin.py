78ca6bfd848d4384de680fa55f1e6cd6bb14f48f
sina rastgouyan <sina.rastgouyan@gmail.com>
2023-06-04 16:26:13 +0430

    Author Model Created & Connetcted to Book Model But It's not Completed yet.

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author
 # Register your models here.
 
 class CustomBooksAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'price']
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name']
 
 admin.site.register(Book, CustomBooksAdmin)
--- books/models.py
+++ books/models.py
@@ -3,9 +3,16 @@ from django.urls import reverse
 
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=360)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=350)
-    author = models.CharField(max_length=350)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='book')
     description = models.TextField() 
     price = models.DecimalField(max_digits=6, decimal_places=2)
     cover = models.ImageField(upload_to='covers/', blank=True)
@@ -16,3 +23,4 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
